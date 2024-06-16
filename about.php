ee2b51d144fb2cc0a39afda7c3cf5d53b7d1f3d3
Nisaur Rohmah <113544206+saurday@users.noreply.github.com>
2024-06-16 17:57:41 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #518EEE"><b>Tentang Toko Souvenir</b></h2>
+	<p class="text-justify">Toko Souvenir adalah salah satu pelopor dalam bisnis souvenir khas Indonesia. Kami menyediakan berbagai macam souvenir unik dan berkualitas tinggi yang mencerminkan keindahan dan keragaman budaya Indonesia. Produk kami tidak hanya menarik, tetapi juga terjangkau oleh semua kalangan, menjadikan setiap momen berkesan dengan sentuhan khas Nusantara.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ footer.php
@@ -0,0 +1,28 @@
+
+	<footer style="border-top: 4px solid #518EEE;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #518EEE"><b>TOKO SOUVENIR</b></h3>
+					<p>Jl. Telang Indah, Kamal, Bangkalan</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +628123456789</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> toko_souvenir@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+	</footer>
+
+</body>
+</html>
